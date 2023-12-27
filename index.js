000970dc1e96f812fbafc79bc560c9d92d9e19a2
MEVINK <123mevinkammalil@gmail.com>
2023-12-27 15:02:16 +0530

    Backend for resturent

---
+++ index.js
@@ -0,0 +1,28 @@
+// import  json-server
+const jsonServer=require('json-server')
+
+// create a server application using jsonServer
+
+const restServer= jsonServer.create()
+
+// setup path for db.json
+
+const router = jsonServer.router('db.json')
+
+// return a middleware used by json server
+
+const middleware= jsonServer.defaults() 
+// setup port number
+
+ const port = 3001
+
+ //use in server
+
+ restServer.use(middleware)
+ restServer.use(router)
+
+ // to run port 
+ restServer.listen(port,()=>{
+    console.log('Rest server listening on port'+port);
+ })
+ 
